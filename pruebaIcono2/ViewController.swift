3332a51241aaa95d6be24d9f91d9859d71fd960d
ThRangel <the.beatles.lma2@gmail.com>
2020-01-11 20:36:53 -0600

    Inicio

---
+++ pruebaIcono2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pruebaIcono2
+//
+//  Created by MacBook Air  on 11/01/20.
+//  Copyright © 2020 MacBook Air . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
